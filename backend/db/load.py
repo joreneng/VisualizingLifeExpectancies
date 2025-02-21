3f2faa5282c91acf3a50abdd74b63035878d6330
Jorene Ng <jorene.ng.b@gmail.com>
2025-02-21 14:23:39 -0800

    Fix bug.....

--- backend/db/load.py
+++ backend/db/load.py
@@ -1,15 +1,18 @@
-from backend.db.setup import process_and_populate_data
+from backend.db.setup import process_and_populate_data, create_table
 from backend.config import INDICATORS
 
 def load_db():
+    # Create table in db
+    create_table()
+
     # Life expectancy at birth, total (years)
     process_and_populate_data(INDICATORS["LIFE_EXPECTANCY"], 1960, 2025)
 
     # Current health expenditure (% of GDP)
     process_and_populate_data(INDICATORS["HEALTH_EXPENDITURE"], 1960, 2025)
 
-    # # Cause of death, by communicable diseases and maternal,
-    # # prenatal and nutrition conditions (% of total)
+    # Cause of death, by communicable diseases and maternal,
+    # prenatal and nutrition conditions (% of total)
     process_and_populate_data(INDICATORS["DEATH_COMM_DISEASES"], 2000, 2025)
 
     # Cause of death, by injury (% of total)
